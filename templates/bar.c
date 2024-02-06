bc5f23f902127be5be846d5e9a30e052bce6a4d6
anu4619 <125575070+anu4619@users.noreply.github.com>
2024-02-06 06:31:47 +0000

    Initial commit

---
+++ templates/bar.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+
+    int a=10,b=20,p,q,r;
+    p=add(a,b);
+    q=mult(a,b);
+    r=sub(a,b);
+printf("%d\n%d\n%d", p,q,r);
+return 0;
+}
+
+
+int add(x,y)
+{
+    int sum=x+y
+    return sum;
+}
+
+int mult(x,y)
+{
+    int pro==x*y;
+    return pro;
+}
+
+int sub(x,y)
+{
+     diff=x-y;
+    return diff;
+}
+
